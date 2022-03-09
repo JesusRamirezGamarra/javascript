7364b5b95f56d85b47bc189a4356fda98414bec4
Jesus Ramirez <jesusramirez@MacBook-Pro-de-lucio.local>
2022-03-08 21:35:46 -0500

    Workshop

--- clase/clase-11-workshop/script.js
+++ clase/clase-11-workshop/script.js
@@ -1,68 +1,60 @@
 class User {
-    cosntructor(nombre, email , password){
-        nombre = this.nombre;
-        email = this.email;
-        password = this.password;
+    constructor(nombre, email, password) {
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
     }
-
 }
 
-let usuarios 
-if(localStorage.getItem('Users')){
-    usuarios = JSON.parse(localStorage.getItem('Users'));
-}
-else{
-    usuarios = []
+let usuarios = []
+
+if(localStorage.getItem('Users')) {
+    usuarios = JSON.parse(localStorage.getItem('Users'))
+} else {
     localStorage.setItem('Users', JSON.stringify(usuarios))
 }
 
+let formUsers = document.getElementById('formUser')
+let botonUsers = document.getElementById('botonUsers')
+let divUsers = document.getElementById('divUsers')
 
-let formUsers = document.getElementById('formUsers');
-let botonUsers = document.getElementById('botonUsers');
-let divUsers = document.getElementById('divUsers');
-
-formUsers.addEventListener('submit',(e) =>{
-    e.preventDefault();
-    let nombre = document.getElementById('usernameID').value
-    let email = document.getElementById('emailID').value
-    let password = document.getElementById('passwordID').value
-
-    const user = new User(nombre, email,password)
+formUsers.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let nombre = document.getElementById('usernameID').value 
+    let email = document.getElementById('emailID').value 
+    let password = document.getElementById('passwordID').value 
+    const user = new User(nombre, email, password)
     usuarios.push(user)
 
-    localStorage.setItem('Users',JSON.stringify(usuarios))
+    localStorage.setItem('Users', JSON.stringify(usuarios))
     formUsers.reset()
-
-
 })
-// otra forma Prefijo on  + Nombre del evento
 
-botonUsers.addEventListener('click',() =>{
-    divUsers.innerHTML ="";
-    usuarios.forEach((usuariosEnArray, indice , array ) =>{
-        console.log(array);
-        divUsers.innerHTML + `
-            <div id="usuario_"${indice} class="card text-white bg-primary mb-3" style="max-width: 20rem;margin=10px">
-                <div class="card-header"><h4>Usuario : ${usuariosEnArray.nombre}}</h4></div>
-                <div class="card-body">
-                    <p class="card-title">Email : ${usuariosEnArray.email}</p>
-                    <button id="boton_${indice}" class="btn btn-dark">Eliminar</button>
+botonUsers.addEventListener('click', () => {
+    divUsers.innerHTML = ""
+
+    usuarios.forEach((usuariosEnArray, indice, array) => {
+        console.log(array)
+
+        divUsers.innerHTML += `
+            <div id="usuario${indice}" class="card border-secondary mb-3" style="max-width: 20rem;margin:10px">
+                <div class="card-header"><h4>Usuario ${usuariosEnArray.nombre}</h4></div>
+                    <div class="card-body">
+                        <p class="card-title">Email: ${usuariosEnArray.email}</p>
+                        <button id="boton${indice}" class="btn btn-danger">Eliminar</button>
+                    </div>
                 </div>
-            </div>
         `
     })
-// }
-
-    usuarios.forEach((usuariosEnArray, indice , array ) => {
-
-        document.getElementById(`boton_${indice}`).addEventListener('click',() => {
-            divUsers.removeChild(document.getElementById(`usuario_${indice}`))
-            let indiceArray = usuarios.finIndex(user =>user.nombre == usuariosEnArray.nombre)
-            usuarios.splice(indiceArray,1)
-            console.log(usuarios)
-            localStorage.setItem('Users', JSON.stringify(usuarios))
-
-        })
+    //usuario.nombreAtributo = nuevoAtributo
+    usuarios.forEach((usuariosEnArray, indice) => {
+       document.getElementById(`boton${indice}`).addEventListener('click', () => {
+           divUsers.removeChild(document.getElementById(`usuario${indice}`))
+           let indiceArray = usuarios.findIndex(user => user.nombre == usuariosEnArray.nombre)
+           usuarios.splice(indiceArray,1)
+           localStorage.setItem('Users', JSON.stringify(usuarios))
+       })
     })
 })
 
+//on + nombre del evento
