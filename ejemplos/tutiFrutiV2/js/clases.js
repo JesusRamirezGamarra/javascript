97ee99ce5795be02f21315545e7a8bb9272050ef
JesusRamirezGamarra <luciojesusramirezgamarra@gmail.com>
2022-07-19 07:43:13 -0500

    Desafio 04-Server Express CRUD API RESTful Mulder

---
+++ ejemplos/tutiFrutiV2/js/clases.js
@@ -0,0 +1,34 @@
+// CONSTRUCTORES
+
+
+class Jugada {
+    constructor (letra, jugadaJugador1, puntosJugador1, puntosTotalesJugador1, jugadaJugador2, puntosJugador2, puntosTotalesJugador2){
+        this.letra=letra,
+        this.jugadaJugador1=jugadaJugador1;
+        this.puntosJugador1=puntosJugador1;
+        this.puntosTotalesJugador1=puntosTotalesJugador1;
+        this.jugadaJugador2=jugadaJugador2;
+        this.puntosJugador2=puntosJugador2;
+        this.puntosTotalesJugador2=puntosTotalesJugador2;
+    }
+} 
+
+class JugadorRankin {
+    constructor (nombre, puntos, cantJugadas, promedio){
+        this.nombre = nombre;
+        this.puntos= puntos;
+        this.cantJugadas=cantJugadas;
+        this.promedio=promedio;
+    }
+}
+
+class JugadaJugador {
+    constructor(nombres, animales, colores, lugares, comidas, objetos){
+        this.nombres=nombres;
+        this.animales=animales;
+        this.colores=colores;
+        this.lugares=lugares;
+        this.comidas=comidas;
+        this.objetos=objetos;
+    }
+}
